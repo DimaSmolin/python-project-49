373d461e82f7d661874d7af2aadd8fcf14ee5cd1
DimaSmolin <dimakoldien@mail.ru>
2022-09-28 09:10:01 +0700

    brain_games/engine.py

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -13,13 +13,13 @@ def starts_games(correct, game_instructions):
 
     count = 0
     for i in range(number_of_rounds):
-        correct_var = correct.question()
-        answer = prompt.string('Your answer: ')
-        if answer == correct_var:
+        corrct = correct.question()
+        answ = prompt.string('Your answer: ')
+        if answ == corrct:
             print('Correct!')
             count += 1
         else:
-            print(f"'{answer}' is wrong answer ;(. Correct answer was '{correct_var}'")
+            print(f"'{answ}' is wrong answer ;(. Correct answer was '{corrct}'")
             print(f"Let's try again, {name}!")
             break
 
